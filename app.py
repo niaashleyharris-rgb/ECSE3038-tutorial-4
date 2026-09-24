b5d4d1b55430b9abbf751ffc406774e6515fb5d7
Nia-Ashley <niaashleyharris@gmail.com>
2026-09-24 12:06:11 -0500

    task 2: delete device

--- app.py
+++ app.py
@@ -49,3 +49,10 @@ def update_device(name: str, updated_device: Device):
     readings.append(updated_device.model_dump())
     return updated_device
 
+@app.delete("/devices/{name}")
+def delete_device(name:str):
+	for device in readings:
+		if device["name"]==name:
+			readings.remove(device)
+	return {"deleted": name}
+	raise HTTPException(status_code=404, detail ="No device called " + name)    
